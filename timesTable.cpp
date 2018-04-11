aee48bdc781308046900597492a417af4991d11d
Synaesthete <synae@Synaesthete.local>
2018-04-10 22:17:46 -0400

    changed char to item

--- timesTable.cpp
+++ timesTable.cpp
@@ -2,7 +2,7 @@
 //
 //  This is a simple command line program to create a visuospatial times table
 //
-//  It will take up to two command line arguments: 
+//  It will take up to two command line arguments:
 //    ARGUMENT 1: sets the size of the table (default is 5 for 5 x 5)
 //    ARGUMENT 2: sets the character for each item (default is 'o')
 //
@@ -13,8 +13,8 @@
 #include <iostream>
 using namespace std;
 
-char* nChar(char ch, int n);
-char* charSpace(char ch);
+char* nItem(char ch, int n);
+char* itemSpace(char ch);
 
 ////////////////////////////////////////////////////////////////////////////////
 //  MAIN: reads arguments, prints out collections of itmes in nested for-loops
@@ -24,7 +24,7 @@ int main(int argc, char** argv) {
     //default values
     int x = 5;
     char c = 'o';
-    
+
     //command line arguments
     if (argc >= 2) {
         x = atoi(argv[1]);
@@ -39,29 +39,29 @@ int main(int argc, char** argv) {
         for (int j = x - k; j < x+1; j++) { //height of each row
             cout << "   ";
             for (int i = 1; i < x+1; i++) { //width of each column
-                cout << nChar(c,i) << "  "; //collection string
+                cout << nItems(c,i) << "  "; //collection string
             }
             cout << "\n";
         }
         cout << endl;
     }
-    cout << "\n\n";  
+    cout << "\n\n";
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 //  NUMBER OF CHARACTERS: creates a string of a specified number of items
 //
-char* nChar(char ch, int n) {
+char* nItems(char ch, int n) {
     char* str = new char[n*2 + 2];
     for (int i = 0; i < n; i++)
-        strcat(str, charSpace(ch));
+        strcat(str, itemSpace(ch));
     return str;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
-//  CHARACTER SPACE: creates a string of a character (item) followed by a space
+//  CHARACTER SPACE: creates a string of an item (character) followed by a space
 //
-char* charSpace(char ch) {
+char* itemSpace(char ch) {
     char* s = new char[3];
     s[0] = ch;
     s[1] = ' ';
