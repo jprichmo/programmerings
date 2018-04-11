60adfa8907811278b801a60e66e5632bad0c5541
Synaesthete <synae@Synaesthete.local>
2018-04-10 22:06:34 -0400

    Initial commit

---
+++ timesTable.cpp
@@ -0,0 +1,70 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  This is a simple command line program to create a visuospatial times table
+//
+//  It will take up to two command line arguments: 
+//    ARGUMENT 1: sets the size of the table (default is 5 for 5 x 5)
+//    ARGUMENT 2: sets the character for each item (default is 'o')
+//
+//  AUTHOR: Jason R
+//
+////////////////////////////////////////////////////////////////////////////////
+
+#include <iostream>
+using namespace std;
+
+char* nChar(char ch, int n);
+char* charSpace(char ch);
+
+////////////////////////////////////////////////////////////////////////////////
+//  MAIN: reads arguments, prints out collections of itmes in nested for-loops
+//
+int main(int argc, char** argv) {
+
+    //default values
+    int x = 5;
+    char c = 'o';
+    
+    //command line arguments
+    if (argc >= 2) {
+        x = atoi(argv[1]);
+    }
+    if (argc == 3) {
+        c = argv[2][0];
+    }
+
+    //prints out collections of items
+    cout << "\n\n";
+    for (int k = 0; k < x; k++) {           //y-axis separated collections
+        for (int j = x - k; j < x+1; j++) { //height of each row
+            cout << "   ";
+            for (int i = 1; i < x+1; i++) { //width of each column
+                cout << nChar(c,i) << "  "; //collection string
+            }
+            cout << "\n";
+        }
+        cout << endl;
+    }
+    cout << "\n\n";  
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//  NUMBER OF CHARACTERS: creates a string of a specified number of items
+//
+char* nChar(char ch, int n) {
+    char* str = new char[n*2 + 2];
+    for (int i = 0; i < n; i++)
+        strcat(str, charSpace(ch));
+    return str;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+//  CHARACTER SPACE: creates a string of a character (item) followed by a space
+//
+char* charSpace(char ch) {
+    char* s = new char[3];
+    s[0] = ch;
+    s[1] = ' ';
+    s[2] = '\0';
+    return s;
+}
